d63d81fb42cd29f9c41c615d0d3bbcea9cb0e683
yadaniel <yadaniel@gmx.de>
2026-05-06 19:39:37 +0200

    script.py output.txt -> output/output.txt correction

--- scripts/script.py
+++ scripts/script.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 log_line = f"Last run: {datetime.utcnow().isoformat()} UTC\n"
 
-with open("output.txt", "a") as f:
+with open("./output/output.txt", "a") as f:
     f.write(log_line)
 
 print("Appended new log entry")
